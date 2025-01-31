97d2751ac7fd9bcf4e4a3da4c10b707714a639f1
sirrajeevgupta <rajeevsofficials@gmail.com>
2025-01-31 23:37:54 +0530

    cors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(logger);
 
 //Handle options credentials check - before CORS!
 // and fetch cookies credentials requirement
-app.use(credentials);
+//app.use(credentials);
 
 //Cross Origin Resource Sharing
 app.use(cors(corsOptions));
