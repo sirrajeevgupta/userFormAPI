b655da637043b06212bb7eb863f98e1bc3f90317
sirrajeevgupta <rajeevsofficials@gmail.com>
2025-01-31 23:02:38 +0530

    cors issue fix

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -5,6 +5,15 @@ const credentials = (req, res, next) => {
 
   if (allowedOrigins.includes(origion)) {
     res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'PUT, GET, POST, DELETE, OPTIONS'
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
   }
   next();
 };
