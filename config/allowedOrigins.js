c05d6bd45464b801872b54cdda073e89883d5fff
sirrajeevgupta <rajeevsofficials@gmail.com>
2025-01-31 23:17:21 +0530

    cors issue

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   'http://localhost:3500',
   'http://localhost:3000',
   'https://userform-yto1.onrender.com',
+  'https://userformapi.onrender.com',
 ];
 
 module.exports = allowedOrigins;
--- middleware/credentials.js
+++ middleware/credentials.js
@@ -5,7 +5,10 @@ const credentials = (req, res, next) => {
 
   if (allowedOrigins.includes(origion)) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://userform-yto1.onrender.com'
+    );
     res.header(
       'Access-Control-Allow-Methods',
       'PUT, GET, POST, DELETE, OPTIONS'
